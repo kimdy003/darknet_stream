1f300d4127bcf20e2cee418a4b5728472510974b
limcheolsun <skycs97@naver.com>
2020-03-16 07:31:39 +0000

    linux

--- include/darknet.h
+++ include/darknet.h
@@ -146,7 +146,8 @@ struct layer{
     #ifdef THREAD
         void (*forward_thread) (netlayer*);
         //2020 0223 hojin gpu threadbody function add
-        void (*forward_gpu_thread) (netlayer*);
+	//2020 0316 doyoung arguments add
+        void (*forward_gpu_thread) (netlayer*, int id);
     #endif
     void (*backward)  (struct layer, struct network);
     void (*update)    (struct layer, update_args);
