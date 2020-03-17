46ef771be005cf4132d0ee1bcb7788f50dadd508
kimdy003 <kyl123451@naver.com>
2020-03-17 15:16:08 +0900

    free() dump find start

--- examples/classifier.c
+++ examples/classifier.c
@@ -644,9 +644,11 @@ void* predict_classifier2(test * input){
 
             printf("%5.2f%%: %s\n", predictions[index] * 100, names[index]);
         }
+        fprintf(stderr, "rdata != im.data\n");
         if (r.data != im.data)
             free_image(r);
     }
+    fprintf(stderr, "n_loop end\n");
     free_image(im);
     free_network(net);
     free(input);
