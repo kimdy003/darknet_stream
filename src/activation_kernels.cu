0c02663ee6f1d413f5022b2eb4e1ccc0b5af84ee
kimdy003 <kyl123451@naver.com>
2020-03-31 01:59:28 +0900

    .

--- src/activation_kernels.cu
+++ src/activation_kernels.cu
@@ -193,13 +193,13 @@ __global__ void gradient_array_kernel(float *x, int n, ACTIVATION a, float *delt
     if(i < n) delta[i] *= gradient_kernel(x[i], a);
 }
 
-
+#ifdef STREAM
 extern "C" void activate_array_gpu_stream(float *x, int n, ACTIVATION a, int id) 
 {
     activate_array_kernel<<<cuda_gridsize(n), BLOCK, 0 , stream_id(id)>>>(x, n, a);
     check_error(cudaPeekAtLastError());
 }
-
+#endif
 extern "C" void activate_array_gpu(float *x, int n, ACTIVATION a) 
 {
     activate_array_kernel<<<cuda_gridsize(n), BLOCK>>>(x, n, a);
