0d1f0531f053e039a883ea9f4eff1f9f4462f8f7
kimdy003 <kyl123451@naver.com>
2020-04-10 19:46:10 +0900

    .

--- src/thpool.c
+++ src/thpool.c
@@ -401,6 +401,7 @@ static void *thread_do(struct thread *thread_p)
 
 		if (threads_keepalive && thpool_p->jobqueue.front->flag == 1)
 		{
+			fprintf(stderr, "{{{{{{%d}}}}}}}\n", thread_p->id);
 			pthread_mutex_lock(&thpool_p->thcount_lock);
 			thpool_p->jobqueue.front->flag = 0;
 			thpool_p->num_threads_working++;
